d960b959acd6327c78a2b0a145bde068314181b3
Brian Yang <yangbri1@msu.edu>
2025-01-18 23:50:46 +0000

    Was able to retrieve max value from HashMap using Collections.max(charCount.values()) but thinking about how to retrieve the associated key linked to the value ---- TreeMap sorting?

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,7 @@
+import java.util.Map;
+
+import java.util.HashMap;
+import java.util.Collections;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +12,41 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        // create a Map to map a Character key w/ an Integer value
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        // convert given String 'str' to char[] array
+        char[] strToAry = str.toCharArray();
+
+        int i = 0;
+        // loop through char[] ary representation of given 'str'
+        while(i < strToAry.length){
+            // retrieve Character at given iterator/index i
+            Character key = strToAry[i];
+            // if Map charCount does NOT already holds 'key'
+            if(!charCount.containsKey(key)){
+                // .put() key-value pair into Map charCount as an element
+                charCount.put(key, 1);
+            }
+            // otw if Map charCount already DOES have the 'key'
+            else{
+                // update existing key-value pair's value
+                charCount.put(key, charCount.get(key) + 1);
+            }
+            // increment by 1 to keep loop going
+            ++i;
+        }
+        // https://stackoverflow.com/questions/49470423/get-the-highest-values-in-a-hashmap-in-java
+        // extracts max value from charCount
+        int maxValue = Collections.max(charCount.values());
+
+        int j = 0;
+        while(j < charCount.size()){
+            if(charCount.containsValue(maxValue)){
+                return charCount[j];    // bracket notation only seems to work for array
+            }
+            ++j;
+        }
+        return maxValue;
     }
 }
