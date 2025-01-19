5666a1d9418ca2b50fb7edaab16c2aac7c910a9f
Brian Yang <yangbri1@msu.edu>
2025-01-19 00:24:57 +0000

    Attempted to use a Set to gather all keys (non-duplicates). Afterwards, called Set's .toArray(new Character[0]) method to convert Set into an Character[] array --- IMPORTANT: 'new Character[0]' or 'new Character[charSet.size()]' needed to explicitly tell .toArray() to convert fro Object[] to Character[].  --- Consequently, traverse through Character[] array 'keys' at exactly the index where 'maxValue' was found but to no avail... ----- Next up refreshing up on ordering for HashMap vs HashSet vs Array and whether or not order is preserved on conversion among them.

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -2,6 +2,8 @@ import java.util.Map;
 
 import java.util.HashMap;
 import java.util.Collections;
+import java.util.Set;
+import java.util.HashSet;
 
 public class MostCommonCharacter {
     /**
@@ -13,16 +15,23 @@ public class MostCommonCharacter {
      */
     public char recurringChar(String str) {
         // create a Map to map a Character key w/ an Integer value
+        // Note: Map can NOT take in primitive types -- need to use Wrapper Classes to autobox (int -> Integer) for their Object equivalent
         Map<Character, Integer> charCount = new HashMap<>();
 
         // convert given String 'str' to char[] array
         char[] strToAry = str.toCharArray();
+
+        // create a Set --- no duplicates allowed
+        Set<Character> charSet = new HashSet<Character>();
 
         int i = 0;
         // loop through char[] ary representation of given 'str'
         while(i < strToAry.length){
             // retrieve Character at given iterator/index i
             Character key = strToAry[i];
+
+            charSet.add(key);
+
             // if Map charCount does NOT already holds 'key'
             if(!charCount.containsKey(key)){
                 // .put() key-value pair into Map charCount as an element
@@ -40,13 +49,34 @@ public class MostCommonCharacter {
         // extracts max value from charCount
         int maxValue = Collections.max(charCount.values());
 
+        // create an empty int[] array w/ declared size being set to charCount
+        // char[] keys = new char[charCount.size()];  
+
         int j = 0;
+        // iterate via HashMap of key-value pairs for Character & their Integer counts
         while(j < charCount.size()){
+            // upon finding the maxValue within HashMap 'charCount'
             if(charCount.containsValue(maxValue)){
-                return charCount[j];    // bracket notation only seems to work for array
+                // return charCount[j];    // bracket notation only seems to work for array
+                // stop the loop
+                break;
             }
+            // increment by 1 to keep loop going
             ++j;
+            // charCount.get()
         }
-        return maxValue;
+        
+        // utilizing .toArray() method in Set to convert 'charSet' of keys into an Character[] array of keys
+        // Note: 'new Character[0]' or 'new Character[charSet.size()]' is explicitly teling Java to return it as a 'Character[]' converted from 'Object[]'
+        // https://stackoverflow.com/questions/43696716/type-mismatch-cannot-convert-from-element-type-object-to-stringgenerics
+        Character[] keys = charSet.toArray(new Character[0]);
+
+        // attempting to return precise key at instance when maxValue was found ...
+        char maxKey = keys[j+1];
+        return maxKey;
+
+        /* Note: Does the order in a Set preserved when .toArray() is called to convert it to an Array?
+         * How about the order within an HashMap compared to a Set? 
+         */
     }
 }
